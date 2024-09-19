85d9ac1b04c0122816b08eb7bd68c8d2266d3882
Mauro J. Sanchirico III <sanchirico.mauro@gmail.com>
2024-09-19 02:02:02 -0400

    Modify environment

--- .devcontainer/configure_environment.sh
+++ .devcontainer/configure_environment.sh
@@ -1,13 +1,13 @@
-echo "Installing http-server..."
+# echo "Installing http-server..."
 
-npm i -g http-server
+# npm i -g http-server
 
-if [ $? -eq 0 ]; then
-    echo "http-server installed!"
-else
-    echo "Failed to install http-server."
-    exit 1
-fi
+# if [ $? -eq 0 ]; then
+#     echo "http-server installed!"
+# else
+#     echo "Failed to install http-server."
+#     exit 1
+# fi
 
 echo "Installing Poetry..."
 
